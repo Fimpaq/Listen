dc0c6f343d714ed4fa0cc81ccf935ad4d4c35eb2
Felix Regner <fregner@impaq.de>
2021-12-14 14:35:38 +0100

    add IntList

---
+++ src/IntList.java
@@ -0,0 +1,8 @@
+
+public class IntList {
+	private int[] array;
+	
+	public IntList(final int[] array) {
+		this.array = array;
+	}
+}
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 
 public class Main {
 	public static void main(String[] args) {
-		System.out.println("hello world");
+		IntList list = new IntList(new int[] { 2, 4, 6, 8 });
 	}
 }
