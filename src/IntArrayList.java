5813bf7508e58ac1e72c07c80e34996b3ff80ab1
Felix Regner <fregner@impaq.de>
2021-12-14 15:27:16 +0100

    Aufgabe 4 begonnen

--- src/IntArrayList.java
+++ src/IntArrayList.java
@@ -6,7 +6,7 @@ public class IntArrayList {
 	
 	public IntArrayList() {}
 	
-	public IntArrayList(int size) {
+	public IntArrayList(final int size) {
 		this.size = size;
 	}
 	
@@ -14,15 +14,27 @@ public class IntArrayList {
 		this.array = array;
 	}
 	
-	public IntArrayList(Integer... z) { // IntegerArray zu intArray wirklich so kompliziert?!?!?
+	public IntArrayList(final Integer... z) { // IntegerArray zu intArray wirklich so kompliziert?!?!?
 		final int[] ia = new int[z.length];
 		for(int i = 0; i < z.length; i++) {
 			ia[i] = z[i];
 		}
 		this.array = ia;
 	}
+
 	
 	public int[] getArray() {
 		return this.array;
 	}
+	
+	public int getSize() {
+		return this.size;
+	}
+	
+	// ------------- Aufgabe 4
+	public void add(final int value) {
+		
+	}
+	
+	
 }
