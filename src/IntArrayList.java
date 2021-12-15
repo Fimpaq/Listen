69d71036502cd61e51ff0c43af69fbf1e7182cff
Felix Regner <fregner@impaq.de>
2021-12-15 15:04:06 +0100

    removeAt fertig

--- src/IntArrayList.java
+++ src/IntArrayList.java
@@ -2,29 +2,30 @@ import java.util.Iterator;
 
 public class IntArrayList {
 	private int[] array;
-	private int size;
+	private int size = 0; // amount of elements in array
 
 	public IntArrayList() {
-		this.size = 0;
+
 	}
 
 	public IntArrayList(final int size) {
-		this.size = size;
+		this.array = new int[size];
 	}
 
 	public IntArrayList(final int[] array) {
-		this.array = array;
+		this.array = new int[array.length];
+		for (int i = 0; i < array.length; i++) {
+			this.array[i] = array[i];
+		}
 		this.size = array.length;
 	}
 
-	public IntArrayList(final Integer... z) {
-		final int[] ia = new int[z.length];
-		for (int i = 0; i < z.length; i++) {
-			ia[i] = z[i];
+	public IntArrayList(final Integer... array) {
+		this.array = new int[array.length];
+		for (int i = 0; i < array.length; i++) {
+			this.array[i] = array[i];
 		}
-		this.array = ia;
-		this.size = z.length;
-		
+		this.size = array.length;
 	}
 
 	public int[] getArray() {
@@ -35,36 +36,86 @@ public class IntArrayList {
 		return this.size;
 	}
 
-	// ------------- Aufgabe 4
-	public void add(final int value) {
-		int[] ia = new int[array.length + (array.length / 2)];
-		for (int i = 0; i < array.length; i++) {
-			ia[i] = array[i];
+	// ------------- Aufgabe 4 -----------------------
+	// wenn arrayGröße nicht reicht, soll die größe um 50% erhöht werden
+
+	private void checkSize(final int n) {  // für add / addAll
+		if ((this.size + n) > this.array.length) {
+			int al = this.array.length;
+			while ((this.size + n) > al) {
+				al += (al / 2);
+			}
+			final int[] tmp = new int[al];
+			for (int i = 0; i < this.size; ++i) {
+				tmp[i] = this.array[i];
+			}
+			this.array = tmp;
 		}
-		ia[array.length] = value;
-		this.array = ia;
-		this.size = ia.length;
+	}
+	
+	public void add(final int... values) {
+		checkSize(values.length);
+
+		int i = this.size;
+		for (int value : values) {
+			this.array[i] = value;
+			i++;
+		}
+		this.size = i;
 	}
 
-	public void addAll(final int[]... values) {
-		int i = this.size;
-				
-		for (int[] arr : values) {
-			this.size += arr.length;
-		}
-		
-		int[] ia = new int[this.size];
-		for (int j = 0; j < this.array.length; j++) {
-			ia[j] = this.array[j];
-		}
-		
-		for (int[] arr : values) {
-			for (int zahl : arr) {
-					ia[i] = zahl;
-					i++;
+	public void addAll(final int[]... arrays) {
+		for (int[] array : arrays) {
+			checkSize(array.length);
+
+			int i = this.size;
+
+			for (int value : array) {
+				this.array[i] = value;
+				i++;
 			}
-		}		
-		this.array = ia;
+			this.size = i;
+		}
+	}	
+
+	public int remove(final int value) { 
+		int gefunden = 0; 		// das array wird um die anzahl der entferten elemte verkleinert / weglassen?
+		for (int i = 0; i < this.size; i++) {
+			if (array[i] == value) {
+				gefunden++;
+				this.size--;
+			}
+		}
+		
+		int[] tmp = new int[this.array.length - gefunden];
+		for (int i = 0, j = 0; i < this.size; i++, j++) {
+			if (array[i] != value) {
+				tmp[j] = array[i];
+			} else {
+				j--;
+			}
+		}
+		this.array = tmp;
+		return gefunden;
+	}
+	
+	public int removeAt(final int idx) {
+		int ret = 0;
+		int[] tmp = new int[this.array.length - 1];
+		for(int i = 0; i < this.size; i++) {
+			if(this.array[i] != array[idx] ) {
+				tmp[i] = this.array[i];
+			} else {
+				ret = array[idx];
+			}
+		}
+		this.array = tmp;
+		this.size -= 1;
+		return ret;		
+	}
+
+	public int removeAll(final int[] values) { // hier 
+		return 0;
 	}
 
 }
--- src/Main.java
+++ src/Main.java
@@ -2,21 +2,56 @@
 public class Main {
 	public static void main(String[] args) {
 		IntArrayList ia = new IntArrayList(4, 6, 1, 2, 3, 6, 2, 3, 2);
-//		
-//		ia.add(20);
-//		
-//		System.out.println("Arraygröße: " + ia.getSize());
-//		
-//		for(int zahl : ia.getArray()) {
-//			System.out.print(zahl + " ");
-//		}
 		
+		System.out.println("size: " + ia.getSize());
+		for (int zahl : ia.getArray()) {
+			System.out.print(zahl + " ");
+		}
+		System.out.println();
+		
+		ia.add(20,12,34,765,2,4,1,76,23,3,6,13,6,2134,2);
+		
+		System.out.println("size: " + ia.getSize());
+		
+		for(int zahl : ia.getArray()) {
+			System.out.print(zahl + " ");
+		}
+		
+		System.out.println();
+		
+		ia.add(666);
+		
+		System.out.println("size: " + ia.getSize());
+		
+		for(int zahl : ia.getArray()) {
+			System.out.print(zahl + " ");
+		}
+		
+		System.out.println();
 		
 		int[] i = { 1, 2, 3 };
 		int[] o = { 5, 6, 7 };
 
 		ia.addAll(i, o);
+		
+		System.out.println("size: " + ia.getSize());
 
+		for (int zahl : ia.getArray()) {
+			System.out.print(zahl + " ");
+		}
+		
+		System.out.println();
+		
+		System.out.println("Es wurden " + (ia.remove(3)) + " Elemente entfernt.");
+		
+		for (int zahl : ia.getArray()) {
+			System.out.print(zahl + " ");
+		}
+		
+		System.out.println();
+		
+		System.out.println("Es wurde die Zahl " + (ia.removeAt(21)) + " entfernt.");
+		
 		for (int zahl : ia.getArray()) {
 			System.out.print(zahl + " ");
 		}
