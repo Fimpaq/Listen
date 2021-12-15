c4bb6d97fedeb7c4702cb966a7a1d75952b7839b
Felix Regner <fregner@impaq.de>
2021-12-15 11:02:59 +0100

    varargs an arrays funktioniert

--- src/IntArrayList.java
+++ src/IntArrayList.java
@@ -1,40 +1,70 @@
+import java.util.Iterator;
 
 public class IntArrayList {
 	private int[] array;
 	private int size;
-	
-	
-	public IntArrayList() {}
-	
+
+	public IntArrayList() {
+		this.size = 0;
+	}
+
 	public IntArrayList(final int size) {
 		this.size = size;
 	}
-	
+
 	public IntArrayList(final int[] array) {
 		this.array = array;
+		this.size = array.length;
 	}
-	
-	public IntArrayList(final Integer... z) { // IntegerArray zu intArray wirklich so kompliziert?!?!?
+
+	public IntArrayList(final Integer... z) {
 		final int[] ia = new int[z.length];
-		for(int i = 0; i < z.length; i++) {
+		for (int i = 0; i < z.length; i++) {
 			ia[i] = z[i];
 		}
 		this.array = ia;
+		this.size = z.length;
+		
 	}
 
-	
 	public int[] getArray() {
 		return this.array;
 	}
-	
+
 	public int getSize() {
 		return this.size;
 	}
-	
+
 	// ------------- Aufgabe 4
 	public void add(final int value) {
-		
+		int[] ia = new int[array.length + (array.length / 2)];
+		for (int i = 0; i < array.length; i++) {
+			ia[i] = array[i];
+		}
+		ia[array.length] = value;
+		this.array = ia;
+		this.size = ia.length;
 	}
-	
-	
+
+	public void addAll(final int[]... values) {
+		int i = this.size;
+				
+		for (int[] arr : values) {
+			this.size += arr.length;
+		}
+		
+		int[] ia = new int[this.size];
+		for (int j = 0; j < this.array.length; j++) {
+			ia[j] = this.array[j];
+		}
+		
+		for (int[] arr : values) {
+			for (int zahl : arr) {
+					ia[i] = zahl;
+					i++;
+			}
+		}		
+		this.array = ia;
+	}
+
 }
--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,24 @@
 
 public class Main {
 	public static void main(String[] args) {
-		IntArrayList ia = new IntArrayList(4,6,1,2,3,6,2,3,2);
-		int[] array = ia.getArray();
-		for(int zahl : array) {
-			System.out.println(zahl);
+		IntArrayList ia = new IntArrayList(4, 6, 1, 2, 3, 6, 2, 3, 2);
+//		
+//		ia.add(20);
+//		
+//		System.out.println("Arraygröße: " + ia.getSize());
+//		
+//		for(int zahl : ia.getArray()) {
+//			System.out.print(zahl + " ");
+//		}
+		
+		
+		int[] i = { 1, 2, 3 };
+		int[] o = { 5, 6, 7 };
+
+		ia.addAll(i, o);
+
+		for (int zahl : ia.getArray()) {
+			System.out.print(zahl + " ");
 		}
 	}
 }
